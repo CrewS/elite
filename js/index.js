6ec726691f801db6afb4c536f2233ab9483834ef
Setcina <1349855697@qq.com>
2018-06-13 17:56:56 +0800

    filebox

--- js/index.js
+++ js/index.js
@@ -7,19 +7,10 @@
     
         return (
             <div>
-            <antd.Transfer
-                dataSource={mockData}
-                titles={['Source', 'Target']}
-                targetKeys={state.targetKeys}
-                selectedKeys={state.selectedKeys}
-                onChange={this.handleChange}
-                onSelectChange={this.handleSelectChange}
-                onScroll={this.handleScroll}
-                render={item => item.title}
-            />
-            <Header name="123" />
-            <Sidebar name="123" />
-            <Footer name="123" />
+                <Header name="123" />
+                <Sidebar name="123" />
+                <Footer name="123" />
+                <DropBoxFile />
             </div>
             );
         }

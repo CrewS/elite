2a35c25327582f49140e1ae5dce7938aef040d3d
Crews <179455570@qq.com>
2018-06-14 16:18:42 +0800

    解决bug

--- js/index.js
+++ js/index.js
@@ -5,17 +5,8 @@
       render() {
         return (
           <div>
-<<<<<<< HEAD
             <Header name="123" />
             <DropBoxFile />
-=======
-            <div>
-                <Header name="123" />
-                <Sidebar name="123" />
-                <Footer name="123" />
-                <DropBoxFile />
-            </div>
->>>>>>> master
             <Footer name="123" />
           </div>
         );

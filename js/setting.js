b7cb52cc4838d9356f2c4fa51505dacf1f17df73
ufresh2013 <ufresh2013@163.com>
2018-06-20 14:32:34 +0800

    管理员交互

--- js/setting.js
+++ js/setting.js
@@ -111,6 +111,7 @@ class Setting extends React.Component {
     pageCurrent: 1,
     pageTotal: 1,
     visible: false,
+    visibleTransfer: false,
     nomoreStaff: false,
     section: 'staff'
   }
@@ -119,7 +120,8 @@ class Setting extends React.Component {
     this.getAdminsList(1, 10)
   }
 
-  // 获取文件夹管理员列表
+  // 1. 文件夹管理员
+  // 1.1 文件夹管理员 - 获取列表
   getAdminsList = (page, page_size) => {
     $.ajax({
       xhrFields: {withCredentials: true},
@@ -139,14 +141,21 @@ class Setting extends React.Component {
     })
   }
 
-  // 查看文件明细
-  showSection = (section) => {
+  // 1.2 文件夹管理员 - 页码变更
+  handlePageChange = (page, pageSize) => {
     this.setState({
-      section: section
+      pageCurrent: page,
     })
   }
 
-  // 弹出添加文件管理员弹框
+  handlePageSizeChange = () => {
+    console.log('pagesizechange')
+  }
+
+
+
+  // 2. 添加文件夹管理员
+  // 2.1 弹出弹框
   showModal = () => {
     this.setState({
       visible: true,
@@ -274,22 +283,17 @@ class Setting extends React.Component {
     }, 2000)
   }
 
-  // 弹出移交文件弹框
-  showTransferModal = () => {
-
-  }
-
-  // 搜索人员
+  // 2.2 搜索人员
   onSearchChange = (e) => {
     console.log(e.target.value)
   }
 
-  // 确定添加已选人员
+  // 2.3 添加人员
   handleOk = () => {
     clearInterval(this.timer)
   }
 
-  // 取消弹框
+  // 2.4 取消弹框
   handleCancel = () => {
     this.setState({
       visible: false,
@@ -298,14 +302,62 @@ class Setting extends React.Component {
 
   }
 
-  // 页码-变更
-  handlePageChange = (page, pageSize) => {
+
+
+  // 3. 移交文件
+  // 3.1 弹出弹框
+  showTransferModal = () => {
     this.setState({
-      pageCurrent: page,
+      visibleTransfer: true,
+    })
+  }
+
+  // 3.2 确定移交
+  handleTransferOk = () => {
+
+  }
+
+  // 3.3 取消弹框
+  handleTransferCancel = () => {
+    this.setState({
+      visibleTransfer: false,
+    })
+  }
+
+
+
+  // 4. 删除管理员 - 二次确认
+  deleteAdmins = (id) => {
+    antd.Modal.confirm({
+      title: '提示',
+      content: '是否确认删除此文件管理员？',
+      iconType: 'exclamation-circle',
+      className: 'confirm-red',
+      onOk: () => {
+        // 删除管理员
+        if (false) {
+          antd.message.success('删除成功');
+        } else {
+          antd.Modal.warning({
+            title: '提示',
+            content: '删除失败！请先把文件夹管理员的文件移交给其他人',
+            okText: '确定',
+          })
+        }
+      }
+    });
+
+  }
+
+  // 5. 页面跳转 - 文件明细
+  showSection = (section) => {
+    this.setState({
+      section: section
     })
   }
 
   render() {
+    // 文件夹管理员 - 列表头
     const columns = [{
       title: '真实姓名',
       dataIndex: 'username',
@@ -332,15 +384,16 @@ class Setting extends React.Component {
       },
     }, {
       title: '操作',
-      dataIndex: 'op',
-      render: () => (
+      dataIndex: 'id',
+      render: (text) => (
         <span>
           <antd.Icon type="swap" style={{ fontSize: '18px', color: '#0692e1', marginRight: '10px' }} onClick={this.showTransferModal} />
-          <antd.Icon type="delete" style={{ fontSize: '18px', color: '#f74953' }} />
+          <antd.Icon type="delete" style={{ fontSize: '18px', color: '#f74953' }} onClick={this.deleteAdmins.bind(this, text)} />
         </span>
       )
     }];
 
+    // 所有员工 - 列表头
     const staffColumns = [{
       title: '真实姓名',
       dataIndex: 'name',
@@ -356,6 +409,32 @@ class Setting extends React.Component {
     }, {
       title: '部门',
       dataIndex: 'place',
+      width: '260px'
+    }];
+
+    // 更改文件夹管理员 - 列表头
+    const transferColumns = [{
+      title: '',
+      dataIndex: 'id',
+      width: '60px',
+      render: (text) => (
+        <span style={{ textAlign: 'center' }}><antd.Radio></antd.Radio></span>
+      )
+    }, {
+      title: '真实姓名',
+      dataIndex: 'username',
+      width: '115px',
+    }, {
+      title: '用户邮箱',
+      dataIndex: 'email',
+      width: '175px',
+    }, {
+      title: '手机',
+      dataIndex: 'phone',
+      width: '145px',
+    }, {
+      title: '部门',
+      dataIndex: 'department',
       width: '260px'
     }];
 
@@ -385,25 +464,36 @@ class Setting extends React.Component {
                 <h1 style={{ fontSize: '16px' }}>网盘设置</h1>
                 <antd.Button type="primary" icon="file-text" className="setHeaderBtn" onClick={this.showSection.bind(this, 'doc')}>文件明细</antd.Button>
               </div>
-              <div style={{ border: '1px solid #eaeaea', borderRadius: '5px' }}>
-                <div style={{ margin: '10px'}}>
-                  <antd.Button icon="plus-square" type="primary" onClick={this.showModal}>添加文件管理员</antd.Button>
-                </div>
+              <div>
                 <antd.Table
                   columns={columns}
                   dataSource={this.state.managerList}
                   bordered
                   pagination={false}
                   size="small"
+                  title={() => <antd.Button icon="plus-square" type="primary" onClick={this.showModal}>添加文件管理员</antd.Button>}
                 />
-                <antd.Pagination
-                  size="small"
-                  current={this.state.pageCurrent}
-                  total={this.state.pageTotal}
-                  onChange={this.handlePageChange}
-                  style={{ margin: '15px 0', textAlign: 'center'}}
-                  showSizeChanger
-                />
+                <div>
+                  <antd.Pagination
+                    size="small"
+                    current={this.state.pageCurrent}
+                    total={this.state.pageTotal}
+                    onChange={this.handlePageChange}
+                    style={{ margin: '15px 0', textAlign: 'center'}}
+                  />
+                  <span>
+                    每页显示
+                    <antd.Select defaultValue="10" size="small" onChange={this.handlePageSizeChange} style={{ margin: '0 5px'}}>
+                      <antd.Select.Option value="10">10</antd.Select.Option>
+                      <antd.Select.Option value="20">20</antd.Select.Option>
+                      <antd.Select.Option value="30">30</antd.Select.Option>
+                      <antd.Select.Option value="50">50</antd.Select.Option>
+                    </antd.Select>
+                    条
+                  </span>
+
+                </div>
+
 
                 {/* 添加文件夹管理员Modal */}
                 <antd.Modal
@@ -421,7 +511,7 @@ class Setting extends React.Component {
                     prefix={<antd.Icon type="search" style={{ color: 'rgba(0,0,0,.25)' }} />}
                     placeholder="请输入真实姓名/用户邮箱/手机/部门"
                     size="default"
-                    style={{ marginBottom: '15px' }}
+                    style={{ marginBottom: '15px', height: '30px' }}
                     onChange={this.onSearchChange}
                   />
                   <antd.Table
@@ -450,11 +540,10 @@ class Setting extends React.Component {
                   bodyStyle={{height:'500px'}}
                 >
                   <antd.Table
-                    columns={staffColumns}
-                    dataSource={this.state.staffList}
+                    columns={transferColumns}
+                    dataSource={this.state.managerList}
                     bordered
                     pagination={false}
-                    rowSelection={rowSelection}
                     scroll={{ y: 350}}
                     size="small"
                     onscroll={this.onscroll}
